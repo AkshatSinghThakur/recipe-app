643d51d5c641dbd22925c340fc79eec62cc5aa7c
Akshat Singh Thakur <akshatthakur7424@gmail.com>
2024-06-20 12:24:01 +0530

    adding image and other data  to the dishes page

--- src/app/dishes/page.js
+++ src/app/dishes/page.js
@@ -23,13 +23,39 @@ export default async function ShowDishes() {
         <div className="h-16 w-screen bg-blue-800">
           <p className="text-3xl text-center p-4 text-white"> Dishes </p>
         </div>
-        <div className="w-full h-auto grid grid-cols-4 p-4 gap-3">
+
+        <div className="w-full h-auto grid grid-cols-4 gap-4">
+
           {dishes && dishes.length > 0 ? (
             dishes.map((dish) => (
               <Link href={`/dishes/${dish.id}`}>
-                <div className="w-72 h-16 flex justify-center items-center bg-white rounded-md shadow-md border-2 border-blue-950 cursor-pointer">
-                  <button key={dish.id}>{dish.name}</button>
+
+                <div className="w-64 h-auto flex flex-col mx-auto mt-8 bg-white rounded-md shadow-md border border-black-950 cursor-pointer ">
+
+                  <div className="w-full">
+                    {" "}
+                    <img
+                      src={`${dish.image}`}
+                      alt={`${dish.name} image`}
+                      className="w-full h-46 rounded-t-md"
+                    />{" "}
+                  </div>
+
+                  <div className="w-full h-14  flex items-center pl-4">
+                    <p key={dish.id} className="text-md font-semibold " >{dish.name}</p>
+                  </div>
+
+                  <div className="w-full h-12 flex justify-around items-center ">
+                    <div>
+                      <p className="text-gray-900 text-sm" >Rating : {dish.rating}</p>
+                    </div>
+                    <div>
+                      <p className="text-gray-900 text-sm" >Cuisine : {dish.cuisine}</p>
+                    </div>
+                  </div>
+
                 </div>
+
               </Link>
             ))
           ) : (
@@ -42,6 +68,8 @@ export default async function ShowDishes() {
             </div>
           )}
         </div>
+
+
       </div>
     </>
   );
